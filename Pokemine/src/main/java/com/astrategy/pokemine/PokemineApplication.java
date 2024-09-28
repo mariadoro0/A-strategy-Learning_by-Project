94d4332b5c894f25c3f3df8358c30e817e8708f5
Maria Doro <mariadoro909@gmail.com>
2024-09-28 11:01:53 +0200

    creazione progetto Java

---
+++ Pokemine/src/main/java/com/astrategy/pokemine/PokemineApplication.java
@@ -0,0 +1,13 @@
+package com.astrategy.pokemine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemineApplication.class, args);
+    }
+
+}
---
+++ Pokemine/src/test/java/com/astrategy/pokemine/PokemineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astrategy.pokemine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
