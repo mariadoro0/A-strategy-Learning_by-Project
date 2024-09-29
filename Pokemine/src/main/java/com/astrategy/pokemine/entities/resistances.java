5ba3649985f07b8bee10aaa51e5aa95e264d996f
Fabio Meloni <fabio.meloni@edu.itspiemonte.it>
2024-09-29 15:32:12 +0200

    TI PREGO

--- Pokemine/src/main/java/com/astrategy/pokemine/entities/resistances.java
+++ Pokemine/src/main/java/com/astrategy/pokemine/entities/resistances.java
@@ -1,5 +1,19 @@
 package com.astrategy.pokemine.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table (name="resistances")
+
 public class resistances {
+	
+	private int id;
+	private String type;
+	private String value;
 
 }
