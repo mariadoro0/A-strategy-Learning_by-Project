1606e7d2d95e51661995949c37bf15dd8bf4208e
BalmaneAyoub <ayoub.balmane@edu.itspiemonte.it>
2024-09-29 15:08:43 +0200

    non mi picchiare pls

--- Pokemine/src/main/java/com/astrategy/pokemine/entities/ability.java
+++ Pokemine/src/main/java/com/astrategy/pokemine/entities/ability.java
@@ -1,40 +1,21 @@
 package com.astrategy.pokemine.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter 
+@Entity
+@Table(name="abilities")
 public class ability{
-	
+
 	private int id ;
 	private String name;
 	private String text;
 	private String type;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	
-	
-	
+
 	
 
 }
