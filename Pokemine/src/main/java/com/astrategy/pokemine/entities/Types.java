789b59bbe15d9eda7a7ed7d1c6b64565273a9d10
Maria Doro <mariadoro909@gmail.com>
2024-09-29 17:05:11 +0200

    correzione errori scrittura e aggiunta annotazione @Id

--- Pokemine/src/main/java/com/astrategy/pokemine/entities/Types.java
+++ Pokemine/src/main/java/com/astrategy/pokemine/entities/Types.java
@@ -1,6 +1,7 @@
 package com.astrategy.pokemine.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +11,7 @@ import lombok.Setter;
 @Entity
 @Table(name="types")
 public class Types {
-	
+	@Id
 	private int id;
 	private String name;
 	
