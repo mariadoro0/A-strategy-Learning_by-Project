4f82b9d10936d024c5d6a84cdcb4fb88350ca994
Fabio Meloni <fabio.meloni@edu.itspiemonte.it>
2024-09-29 14:44:06 +0200

    Branch attacks

--- Pokemine/src/main/java/com/astrategy/pokemine/entities/attacks.java
+++ Pokemine/src/main/java/com/astrategy/pokemine/entities/attacks.java
@@ -6,6 +6,19 @@ public class attacks {
 	private String card_id;
 	private String attack_id;
 	
+	public String getCard_id() {
+		return card_id;
+	}
+	public void setCard_id(String card_id) {
+		this.card_id = card_id;
+	}
+	public String getAttack_id() {
+		return attack_id;
+	}
+	public void setAttack_id(String attack_id) {
+		this.attack_id = attack_id;
+	}
+	
 	
 	
 
