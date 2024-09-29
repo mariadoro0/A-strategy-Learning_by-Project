5c77906462c2c89f3bbba25cf7ca44403afc9b7a
BalmaneAyoub <ayoub.balmane@edu.itspiemonte.it>
2024-09-29 15:23:07 +0200

    chiedo perdono

---
+++ Pokemine/src/main/java/com/astrategy/pokemine/entities/weaknesses.java
@@ -0,0 +1,18 @@
+package com.astrategy.pokemine.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="weakness")
+public class weaknesses {
+	
+	private int id;
+	private String type;
+	private String value;
+
+}
