60832e3b702fc1dd22835daccd09fa4378aca8bc
BalmaneAyoub <ayoub.balmane@edu.itspiemonte.it>
2024-10-02 11:00:27 +0200

    Metodi del dao

---
+++ Pokemine/src/main/java/com/astrategy/pokemine/repos/UsersDAO.java
@@ -0,0 +1,31 @@
+package com.astrategy.pokemine.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.yaml.snakeyaml.events.Event.ID;
+
+import com.astrategy.pokemine.entities.Users;
+
+@Repository
+public interface UsersDAO extends JpaRepository<Users, Integer>{
+	
+	//Create
+	void addUser(Users user);
+	
+	//Read
+	
+	//List<Users> findbyemail(String email);
+	//findbyname
+	
+	//update
+	void updateUser(Users user);
+	
+	
+	//delete
+	//void deleteById(ID id);	
+	
+	
+	
+}
