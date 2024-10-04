421846a46a5d090cdfd2eef23abefe5204a3e7e2
musty <mustafa.muhagr@edu.itspiemonte.it>
2024-10-04 22:47:54 +0200

    Created Interface CardService. Add: CardDAO findById

--- Pokemine/src/main/java/com/astrategy/pokemine/repos/CardDAO.java
+++ Pokemine/src/main/java/com/astrategy/pokemine/repos/CardDAO.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CardDAO extends JpaRepository<Card, String> {
+    Optional<Card> findById(String Id);
     List<Card> findByGeneration(String generation);
     List<Card> findByArtist(String artist);
     List<Card> findBySetName(String set);
