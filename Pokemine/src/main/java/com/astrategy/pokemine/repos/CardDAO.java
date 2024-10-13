10b8a6a88f7602140951cb4a0367c375ace39f26
Fabio Meloni <fabio.meloni@edu.itspiemonte.it>
2024-10-13 19:02:31 +0200

    Ho apportato delle modifiche ulteriori, dimmi cosa ne pensi

--- Pokemine/src/main/java/com/astrategy/pokemine/repos/CardDAO.java
+++ Pokemine/src/main/java/com/astrategy/pokemine/repos/CardDAO.java
@@ -2,13 +2,14 @@ package com.astrategy.pokemine.repos;
 
 import com.astrategy.pokemine.entities.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CardDAO extends JpaRepository<Card, String> {
+public interface CardDAO extends JpaRepository<Card, String>, JpaSpecificationExecutor<Card> {
     Optional<Card> findById(String Id);
     List<Card> findByGeneration(String generation);
     List<Card> findByArtist(String artist);
--- Pokemine/src/main/java/com/astrategy/pokemine/services/CardService.java
+++ Pokemine/src/main/java/com/astrategy/pokemine/services/CardService.java
@@ -1,21 +1,29 @@
 package com.astrategy.pokemine.services;
 
 import java.util.List;
-import java.util.Optional;
-
 import com.astrategy.pokemine.entities.Card;
 
 public interface CardService {
-	
-        Card getCardById(String id);
-        List<Card> getCardByGeneration(String generation);
-        List<Card> getCardByArtist(String artist);
-        List<Card> getCardBySetName(String set);
-        List<Card> getCardByRarity(String rarity);
-        List<Card> getCardBySupertype(String supertype);
-        List<Card> getCardByType_Id(int typeId);
-        Card getCardByName(String name);
-        List<Card> getAllCards();
-        List<Card> getCardByGenerationByArtistBySetNameByRarityBySupertypeByType_Id(String generation, String artist, String set, String rarity, String supertype, Integer typeId, String name);
-
+    
+    List<Card> getByFilters(String id, String name, String artist, String type, String setName, String generation, String rarity, String supertype, int page);
+    
+    Card getCardById(String id);
+    
+    List<Card> getCardByGeneration(String generation);
+    
+    List<Card> getCardByArtist(String artist);
+    
+    List<Card> getCardBySetName(String set);
+    
+    List<Card> getCardByRarity(String rarity);
+    
+    List<Card> getCardBySupertype(String supertype);
+    
+    List<Card> getCardByTypeId(int typeId);
+    
+    Card getCardByName(String name);
+    
+    List<Card> getAllCards();
+    
+    List<Card> getCardByFilters(String generation, String artist, String set, String rarity, String supertype, Integer typeId, String name);
 }
