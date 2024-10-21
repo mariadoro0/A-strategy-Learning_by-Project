6a8c805c7f7a2335915ed9e947ecf55babfa6a41
Maria Doro <mariadoro909@gmail.com>
2024-10-21 22:36:18 +0200

    stringa  per usare postman

--- Pokemine/src/main/java/com/astrategy/pokemine/security/SecurityConfig.java
+++ Pokemine/src/main/java/com/astrategy/pokemine/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,7 +38,9 @@ public class SecurityConfig{
                       authorize.requestMatchers("/cards/**","/decks/**").hasRole("USER");
                       authorize.anyRequest().authenticated();
                       })
-            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+            .httpBasic(Customizer.withDefaults()) // Enable Basic Authentication
+            // Stateless session management
+            //.formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
             //this part for thymeleaf auth with page
     	//	.formLogin((form)->
     		//	form.loginPage("/login")
